f7bcc1eda6de964efd7bd859d63bee6b675b637c
mybofagit <mkums@hotmail.com>
2026-01-08 17:21:26 +0000

    vcp updates

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "microType" {
   ami           = var.ami
   instance_type = var.vmType
 
+vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
   tags = {
     Name = var.vmName
   }
